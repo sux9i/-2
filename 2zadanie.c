e34bb5af5b9e804326385054713e8d47a596e06c
sux9i <belakinatatana035@gmail.com>
2026-09-23 22:45:33 +0300

    Update 2zadanie.c

--- 2zadanie.c
+++ 2zadanie.c
@@ -1,15 +1,12 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 #include <locale.h>
-int main()
+main()
 {
-	int N = 16;
-	int K = 30;
-	int Y = N * 60 + K;
-	int O = (N - 8) * 3600 + K * 60;
-	setlocale(LC_CTYPE, "RUS");
-	printf("Сейчас %d часов %d минут 00 секунд\n", N, K);
-	printf("Идет %d минута суток\n", Y);
-	printf("До полуночи осталось %d часов и %d минут\n", N,K);
-	printf("С 8.00 прошло %d секунд\n", O);
-	printf("Текущий час  = %d суток  и текущая минута =  %d часа\n", N,O);
+	setlocale(LC_ALL, "RUS");
+	int N=16, K=30;
+	printf("Сейчас %d часов %d минут 00 секунд", N, K);
+	printf("\nИдет %d минута суток", N*60+K);
+	printf("\nДо полуночи осталось %d часов %d минут", 23 - N, 60 - K);
+	printf("\nС 8.00 прошло %d секунд", (N - 8) * 60 * 60 + K * 60);
+	printf("\nТекущий час %.2f суток и текущая минута %.2f часа", N / 24.0, K / 60.0);
 }
